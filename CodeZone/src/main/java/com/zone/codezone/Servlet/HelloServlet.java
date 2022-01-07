7f4c7e14b39c84e085905faf55a7885c32bbb498
KABRANESOUMIA <kabrane.soumia.com>
2022-01-07 11:13:47 +0100

    add config db file

--- CodeZone/src/main/java/com/zone/codezone/Servlet/HelloServlet.java
+++ CodeZone/src/main/java/com/zone/codezone/Servlet/HelloServlet.java
@@ -1,5 +1,7 @@
 package com.zone.codezone.Servlet;
 
+import com.zone.codezone.config.Config;
+
 import java.io.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -9,17 +11,18 @@ public class HelloServlet extends HttpServlet {
     private String message;
     private  String test;
 
+
     public void init() {
         test = "Mousstaef";
         message = "Hello World!";
     }
-
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
         // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
         out.println("<h1>" + message + "</h1>");
+        out.println("<h1>" + Config.getInstance() + "</h1>");
         out.println("<h1>" + test + "</h1>");
         out.println("</body></html>");
     }
