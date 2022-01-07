5f35560d2794125469b11bfb50f6da61ec0d0958
devnart <hr.fb001@gmail.com>
2022-01-07 10:06:01 +0100

    Staff model

--- CodeZone/src/main/java/com/zone/codezone/Models/Staff.java
+++ CodeZone/src/main/java/com/zone/codezone/Models/Staff.java
@@ -1,2 +1,47 @@
-package com.zone.codezone.Models;public class Staff {
+package com.zone.codezone.Models;
+
+public class Staff {
+    private int  id;
+    private String username;
+    private String email;
+    private String password;
+
+    public Staff(int id, String username, String email, String password) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
