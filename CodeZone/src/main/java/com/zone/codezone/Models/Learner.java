75a8e1c59b69d1df54bf54683242c86d1e869e9a
KABRANESOUMIA <kabrane.soumia.com>
2022-01-07 09:27:45 +0100

    add learner model

---
+++ CodeZone/src/main/java/com/zone/codezone/Models/Learner.java
@@ -0,0 +1,48 @@
+package com.zone.codezone.Models;
+
+public class Learner {
+    private int  id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    //private ClassLearner classLearner;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Learner(int id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+}
